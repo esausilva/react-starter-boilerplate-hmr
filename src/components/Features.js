daec74cb795d9a4d29fe78b8cf474eb9864f2fee
Esau Silva <esausilva85@gmail.com>
2017-12-25 14:35:57 -0600

    Updated dependencies

--- src/components/Features.js
+++ src/components/Features.js
@@ -12,7 +12,7 @@ const Features = () => {
     'Stage 1 Preset',
     'Webpack 3',
     'Code splitting by Route and Vendor',
-    '<a href="https://github.com/th0r/webpack-bundle-analyzer" target="_blank">Webpack Bundle Analyzer</a>. (Disabled by default)',
+    '<a href="https://github.com/th0r/webpack-bundle-analyzer" target="_blank">Webpack Bundle Analyzer</a>',
     "Take a look at <a href='https://github.com/esausilva/react-starter-boilerplate-hmr/blob/master/package.json' target='_blank'>package.json</a>"
   ];
 
